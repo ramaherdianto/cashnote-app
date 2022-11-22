e7295195936dd57b914c5a5fec22623e1c4fe31c
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-11-22 15:43:40 +0700

    feat: install plugin daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,27 @@ const App = () => {
 
     return (
         <div className='App'>
+            {/* The button to open modal */}
+            <label htmlFor='my-modal' className='btn'>
+                open modal
+            </label>
+
+            {/* Put this part before </body> tag */}
+            <input type='checkbox' id='my-modal' className='modal-toggle' />
+            <div className='modal'>
+                <div className='modal-box'>
+                    <h3 className='font-bold text-lg'>Congratulations random Internet user!</h3>
+                    <p className='py-4'>
+                        You've been selected for a chance to get one year of subscription to use
+                        Wikipedia for free!
+                    </p>
+                    <div className='modal-action'>
+                        <label htmlFor='my-modal' className='btn'>
+                            Yay!
+                        </label>
+                    </div>
+                </div>
+            </div>
             <div className='title-apps flex justify-center flex-col items-center'>
                 <h1 className='text-[40px] border-b-2 py-6 px-8 uppercase font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#2998FF] to-[#3C3DBF]'>
                     CashNote Apps
