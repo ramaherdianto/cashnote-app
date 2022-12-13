1531a5b0ccd7c226c95ccd5519583165333bc5c3
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-12-13 21:21:10 +0700

    feat: update summary and warning popup

--- src/components/TransactionContent.jsx
+++ src/components/TransactionContent.jsx
@@ -42,7 +42,6 @@ const TransactionContent = ({ tambahTransaksi, summary, incomeTransactions }) =>
                             <button
                                 onClick={handleOpenAlert}
                                 className={`bg-[#FF3666] btn__modal border-none flex items-center gap-2 py-2 px-[10px] rounded-lg text-slate-100 opacity-50`}
-                                // disabled
                             >
                                 <span className='text-sm'>Pengeluaran</span>{' '}
                                 <i className='ri-indeterminate-circle-fill'></i>
@@ -55,15 +54,15 @@ const TransactionContent = ({ tambahTransaksi, summary, incomeTransactions }) =>
                                         id='modal'
                                         className='flex-col opacity-100 justify-center  bg-white py-12 px-24 border-[3px] border-slate-300 rounded-xl '
                                     >
-                                        <div className='flex justify-center items-center'>
+                                        <div className='flex justify-center items-center mb-6'>
                                             <h1
-                                                className={`font-bold text-xl mb-3
+                                                className={`font-bold text-center text-2xl 
                                         text-[#FF3666]`}
                                             >
                                                 Oops!
                                             </h1>
                                         </div>
-                                        <div className='flex flex-col text-lg  text-zinc-600   mb-10'>
+                                        <div className='flex justify-center items-center text-lg  text-zinc-600   mb-10'>
                                             <p
                                                 className={`text-base mb-3
                                                 text-[#A1A1A1] text-center whitespace-pre-wrap`}
